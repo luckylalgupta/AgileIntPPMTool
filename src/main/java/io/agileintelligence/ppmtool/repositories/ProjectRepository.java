0739e0a8270b443d2640b5a41a1cf909f17f18a6
Lucky <luckylalgupta@gmail.com>
2021-01-24 08:14:48 +0530

    Project Entity is committed

---
+++ src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.ppmtool.repositories;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+}
